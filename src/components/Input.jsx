f4b89059877e145929a861ff16418b074b196d95
pongsoyun <thdbstjdud@gmail.com>
2020-08-05 16:31:56 +0900

    :art: css component 별로 깔끔하게 정리 - margin, padding 중첩된부분들 처리 - input 부분 깔끔하게 처리 + 영역구분을위해 div 묶어줌

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,39 +3,41 @@ import React, { useEffect, useState } from 'react';
 export default function Input({ text, menu, handleInputMenu, handleSubmit, changeInputText }) {
 	return (
 		<div className="input-container">
-			<div className="input__menus" onClick={(e) => handleInputMenu(e.target.value)}>
-				<div>
-					<input type="radio" name="menu" id="work" value="work" defaultChecked />
-					<label for="work">Work</label>
+			<div className="input__memos__form">
+				<div className="input__menus" onClick={(e) => handleInputMenu(e.target.value)}>
+					<div>
+						<input type="radio" name="menu" id="work" value="work" defaultChecked />
+						<label for="work">Work</label>
+					</div>
+					<div>
+						<input type="radio" name="menu" id="life" value="life" />
+						<label for="life">Life</label>
+					</div>
+					<div>
+						<input type="radio" name="menu" id="hobby" value="hobby" />
+						<label for="hobby">Hobby</label>
+					</div>
+					<div>
+						<input type="radio" name="menu" id="wish" value="wish" />
+						<label for="wish">Wish</label>
+					</div>
+					<div>
+						<input type="radio" name="menu" id="health" value="health" />
+						<label for="health">Health</label>
+					</div>
+					<div>
+						<input type="radio" name="menu" id="habit" value="habit" />
+						<label for="habit">Habit</label>
+					</div>
 				</div>
-				<div>
-					<input type="radio" name="menu" id="life" value="life" />
-					<label for="life">Life</label>
-				</div>
-				<div>
-					<input type="radio" name="menu" id="hobby" value="hobby" />
-					<label for="hobby">Hobby</label>
-				</div>
-				<div>
-					<input type="radio" name="menu" id="wish" value="wish" />
-					<label for="wish">Wish</label>
-				</div>
-				<div>
-					<input type="radio" name="menu" id="health" value="health" />
-					<label for="health">Health</label>
-				</div>
-				<div>
-					<input type="radio" name="menu" id="habit" value="habit" />
-					<label for="habit">Habit</label>
-				</div>
-			</div>
 
-			<div className="input__memo">
-				<textarea
-					className="input__memo__text"
-					value={text}
-					onChange={(e) => changeInputText(e.target.value)}
-				/>
+				<div className="input__memo">
+					<textarea
+						className="input__memo__text"
+						value={text}
+						onChange={(e) => changeInputText(e.target.value)}
+					/>
+				</div>
 			</div>
 
 			<div className="input__memo__submit">
