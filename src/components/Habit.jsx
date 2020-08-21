a8880acf60c622b7ccc0b22506db2871170ca594
pongsoyun <thdbstjdud@gmail.com>
2020-08-21 22:56:30 +0900

    ArrowButton도 리터럴 상수화

--- src/components/Habit.jsx
+++ src/components/Habit.jsx
@@ -2,24 +2,28 @@ import React from 'react';
 import { useCircularList } from './../hooks/useCircularList';
 import { useInterval } from './../hooks/useInterval';
 
+const LEFT = 'left';
+const RIGHT = 'right';
+const DELAY_TIME = 5000;
+
 const ArrowButton = ({ direction, clickHandler }) => (
 	<button className={`habit__${direction}__btn`} onClick={clickHandler}>
 		<i className={`fa fa-chevron-${direction}`} aria-hidden="true"></i>
 	</button>
 );
+
 export default function Habit({ habits }) {
 	const [habit, before, next] = useCircularList(habits);
-	useInterval(next, 5000);
+	useInterval(next, DELAY_TIME);
 
 	return (
 		<header className="habit">
 			<div className="habit__div">
 				<span className="habbit__text">{habit?.text || '이곳에 습관 텍스트가 나옵니다.'}</span>
 			</div>
-
 			<div className="habit__btn__container">
-				<ArrowButton direction="left" clickHandler={before} />
-				<ArrowButton direction="right" clickHandler={next} />
+				<ArrowButton key={LEFT} direction={LEFT} clickHandler={before} />
+				<ArrowButton key={RIGHT} direction={RIGHT} clickHandler={next} />
 			</div>
 		</header>
 	);
